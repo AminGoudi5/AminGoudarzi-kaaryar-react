d548241871c726eabf87027141f69c416b5a6d76
aminGoudi <godarziamin86772@gmail.com>
2023-12-18 10:30:21 +0330

    06

---
+++ 06/src/Header/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+
+function Header() {
+    return (
+        <div className="header">
+            <p className='headerText'>
+               نظرات
+            </p>
+        </div>
+    )
+}
+
+
+export default Header
